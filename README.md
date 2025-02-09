860d2d2931802fbc1073acc29c234f54083e50e2
Oreva12 <ovedheo@gmail.com>
2025-02-09 13:09:02 +0100

    Done!

---
+++ README.md
@@ -0,0 +1,9 @@
+# Backend API - Stage 0
+
+This is a simple backend API that returns basic information about the developer and the project.
+
+## Setup Instructions
+
+1. Clone the repository:
+```bash
+git clone https://github.com/oreva12/Public-Api.git
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_info():
+    email = "ovedheo@gmail.com"
+    current_datetime = datetime.datetime.now(datetime.UTC) + "Z"
+    github_url = "https://github.com/oreva12/Public-Api.git"
+
+    return jsonify({
+        "email": email,
+        "current_datetime": current_datetime,
+        "github_url": github_url
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
